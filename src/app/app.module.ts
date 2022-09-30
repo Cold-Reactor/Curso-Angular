4cefe5d8ddcd00fa11f9af9d840dd305dd344bbe
Cris Duran <cris.camachoe@gmail.com>
2022-09-30 16:44:59 -0700

    quinta subida en desarrollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
-
-
-
+// Cambiar el local de la app
+import localEs from '@angular/common/locales/es-MX' // Todo: Se importa la libreria del idioma
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localEs); //TODO: Se registra en el la aplicacion para poder declararlo
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,9 +17,12 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     SharedModule,
-    AppRouterModule
+    AppRouterModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-MX'} //TODO: Se declara el lenguaje por defecto de la aplicacion
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
